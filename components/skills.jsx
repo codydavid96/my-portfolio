6e1a8ee35b422b57869a8bd8d0e15bce3a8987d7
Cody Hardman <codydavidh96@gmail.com>
2022-10-11 12:21:14 +1000

    fixed images for skills

--- components/skills.jsx
+++ components/skills.jsx
@@ -1,5 +1,12 @@
 import Image from 'next/image'
 import React from 'react'
+import HTML from '../public/assets/skills/html.png'
+import CSS from '../public/assets/skills/css.png'
+import JavaScript from '../public/assets/skills/javascript.png'
+import reactImg from '../public/assets/skills/react.png'
+import Bootstrap from '../public/assets/skills/bootstrap.png'
+import Tailwind from '../public/assets/skills/tailwind css.png'
+import GitHub from '../public/assets/skills/github.png'
 
 const Skills = () => {
   return (
@@ -15,7 +22,7 @@ const Skills = () => {
             <div className='p-6 bg-gradient-to-br from-[#9055ff] to-[#13e2da] text-[#1f1f1f] shadow-xl shadow-black rounded-full hover:scale-105 ease-in duration-300'>
                 <div className='grid grid-cols-2 gap-4 justify-center items-center'>
                     <div className='m-auto'>
-                        <Image src='/../public/assets/skills/html.png' 
+                        <Image src={HTML} 
                         width='64px' 
                         height='64px' 
                         alt='/' 
@@ -29,7 +36,7 @@ const Skills = () => {
             <div className='p-6 bg-gradient-to-br from-[#9055ff] to-[#13e2da] text-[#1f1f1f] shadow-xl shadow-black rounded-full hover:scale-105 ease-in duration-300'>
                 <div className='grid grid-cols-2 gap-4 justify-center items-center'>
                     <div className='m-auto'>
-                        <Image src='/../public/assets/skills/css.png' 
+                        <Image src={CSS} 
                         width='64px' 
                         height='64px' 
                         alt='/' 
@@ -43,7 +50,7 @@ const Skills = () => {
             <div className='p-6 bg-gradient-to-br from-[#9055ff] to-[#13e2da] text-[#1f1f1f] shadow-xl shadow-black rounded-full hover:scale-105 ease-in duration-300'>
                 <div className='grid grid-cols-2 gap-4 justify-center items-center'>
                     <div className='m-auto'>
-                        <Image src='/../public/assets/skills/javascript.png' 
+                        <Image src={JavaScript}
                         width='64px' 
                         height='64px' 
                         alt='/' 
@@ -57,7 +64,7 @@ const Skills = () => {
             <div className='p-6 bg-gradient-to-br from-[#9055ff] to-[#13e2da] text-[#1f1f1f] shadow-xl shadow-black rounded-full hover:scale-105 ease-in duration-300'>
                 <div className='grid grid-cols-2 gap-4 justify-center items-center'>
                     <div className='m-auto'>
-                        <Image src='/../public/assets/skills/react.png' 
+                        <Image src={reactImg} 
                         width='64px' 
                         height='64px' 
                         alt='/' 
@@ -71,7 +78,7 @@ const Skills = () => {
             <div className='p-6 bg-gradient-to-br from-[#9055ff] to-[#13e2da] text-[#1f1f1f] shadow-xl shadow-black rounded-full hover:scale-105 ease-in duration-300'>
                 <div className='grid grid-cols-2 gap-4 justify-center items-center'>
                     <div className='m-auto'>
-                        <Image src='/../public/assets/skills/bootstrap.png' 
+                        <Image src={Bootstrap} 
                         width='64px' 
                         height='64px' 
                         alt='/' 
@@ -85,7 +92,7 @@ const Skills = () => {
             <div className='p-6 bg-gradient-to-br from-[#9055ff] to-[#13e2da] text-[#1f1f1f] shadow-xl shadow-black rounded-full hover:scale-105 ease-in duration-300'>
                 <div className='grid grid-cols-2 gap-4 justify-center items-center'>
                     <div className='m-auto'>
-                        <Image src='/../public/assets/skills/tailwind css.png' 
+                        <Image src={Tailwind} 
                         width='64px' 
                         height='64px' 
                         alt='/' 
@@ -99,7 +106,7 @@ const Skills = () => {
             <div className='p-6 bg-gradient-to-br from-[#9055ff] to-[#13e2da] text-[#1f1f1f] shadow-xl shadow-black rounded-full hover:scale-105 ease-in duration-300'>
                 <div className='grid grid-cols-2 gap-4 justify-center items-center'>
                     <div className='m-auto'>
-                        <Image src='/../public/assets/skills/github.png' 
+                        <Image src={GitHub}
                         width='64px' 
                         height='64px' 
                         alt='/' 
